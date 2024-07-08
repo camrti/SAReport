7823ca90d6f7713e134ab97a9589cdfb80a976eb
MarcoSellitto <151437717+MarcoSellitto@users.noreply.github.com>
2024-07-08 11:55:29 +0200

    Modified almost everything
    constants.py -> new file with constants
    CST_Group6.py -> modified and corrected name
    functions.py -> much more efficient
    membership.py -> new file for calculation and plotting of membership functions
    person.py -> adapted
    plotting.py -> adapted
    test_cases.py -> now uses csv file users_context_attributes.csv

---
+++ CST_Group6/CST_Group6.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from functions import F_SL_function, F_M_function
+from plotting import plot_step_function, plot_customised_step_function
+from test_cases import week1cases
+from constants import F_FQ_labels, F_NA_labels, F_FQ_values, F_NA_values
+from membership import plot_membership_functions
+
+def main():
+    # Generate graphs
+    # Fuzzified forum questions
+    x_values = np.linspace(0, 3.5, 50)
+    plot_membership_functions(x_values,F_FQ_labels,"Fuzzified Forum Questions","plot_membership_forum_questions.png")
+    # Fuzzified non answered forum questions
+    plot_membership_functions(x_values, F_NA_labels,"Fuzzified Non Answered Forum Questions","plot_membership_not_answered.png")
+    # F_FQ
+    plot_step_function(F_FQ_labels, F_FQ_values, 'F_FQ (Questions on the forum)', 'Questions on the forum', 'plot_F_FQ.png')
+    # F_NA
+    plot_step_function(F_NA_labels, F_NA_values, 'F_NA (Questions without answers)', 'Questions without answers', 'plot_F_NA.png')
+    # F_SL
+    x_values = [0, 4, 7, 10, 14, 18, 21, 24, 28, 35]
+    plot_customised_step_function(x_values,[F_SL_function(x) for x in x_values],'F_SL (Hours spent on the platform)','Hours spent on the platform','plot_F_SL.png')
+    # F_M
+    x_values = [0, 1, 4, 7, 9, 11, 13]
+    plot_customised_step_function(x_values,[F_M_function(x) for x in x_values],'F_M (Usage of provided material)','Usage of provided material','plot_F_M.png')
+
+    # Run test cases
+    week1cases()
+
+# MAIN
+if __name__ == "__main__":
+    main()
---
+++ CST_Group6/membership.py
@@ -0,0 +1,53 @@
+import matplotlib.pyplot as plt
+
+def get_membership_rules(labels):
+    """
+    Function to get membership rules with 3 labels, ranges:
+    - First label descends from 0 to 1
+    - Second label ascends from 0 to 1 and descends from 2 to 3
+    - Third label ascends from 2 to 3
+
+    Parameters:
+    ----------
+    labels
+        Array of 3 string labels
+    
+    Returns:
+    ----------
+    membership_rules
+        Array of y values in [0] and label in [1] for each membership function (3)
+    """
+    if len(labels) != 3:
+        raise ValueError("You need to specify 3 labels")
+    membership_rules = [
+        (lambda x: 1.0 if x <= 0 else (1 - x) if 0 < x < 1 else 0.0, labels[0]),
+        (lambda x: x if 0 < x < 1 else (1.0 if 1 <= x <= 2 else (3 - x if 2 < x < 3 else 0.0)), labels[1]),
+        (lambda x: 0.0 if x <= 2 else (x - 2) if 2 <= x <= 3 else 1.0, labels[2]),
+    ]
+    return membership_rules
+
+# Funzione per plottare le funzioni di appartenenza
+def plot_membership_functions(x_values, labels, title : str, filename : str):
+    """
+    xvalues
+        np array of values for x
+    labels
+        Labels for each membership function (3)
+    title : str
+        Plot title
+    filename : str
+        Filename for saving the file
+    """
+    rules = get_membership_rules(labels)
+    plt.figure(figsize=(10, 6))
+    for rule, label in rules:
+        values = [rule(x) for x in x_values]
+        plt.plot(x_values, values, label=label)
+    plt.title(title)
+    plt.xlabel('x')
+    plt.ylabel('Membership')
+    plt.ylim(-0.1, 1.1)
+    plt.grid(True)
+    plt.legend()
+    plt.savefig(filename)  # Salvataggio del grafico con il nome specificato
+    plt.close()
